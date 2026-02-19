26ede761688c685e5e73e56eb165338e89895ae4
VP1905 <Vpatel0589@conestogac.on.ca>
2026-02-19 17:37:42 -0500

    Removed Dev enviornment to enable all the enviornment to run the application.

--- CarRentalApplication/Program.cs
+++ CarRentalApplication/Program.cs
@@ -18,12 +18,11 @@ builder.Services.AddHttpClient("MaintenanceApi", (sp, client) =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
