4717088068cb4714c5c8c165c1365ec3cf22eb33
VP1905 <Vpatel0589@conestogac.on.ca>
2026-04-12 21:39:26 -0400

    Vehicle Inventory is alter for this assignment.

---
+++ VehicleInventory.WebAPI_VP/Controllers/VehiclesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleInventory.WebAPI_VP.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
